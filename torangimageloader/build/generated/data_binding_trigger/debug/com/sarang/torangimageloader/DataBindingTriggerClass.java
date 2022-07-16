588c62c08ca905f3f22d7804bead48570f428ee3
Sarang Yang <sarang628@naver.com>
2022-07-16 12:29:22 +0900

    update

--- torangimageloader/build/generated/data_binding_trigger/debug/com/sarang/torangimageloader/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.sarang.torangimageloader;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
--- torangimageloader/build/generated/source/kapt/debug/com/sarang/torangimageloader/BR.java
+++
@@ -1,5 +0,0 @@
-package com.sarang.torangimageloader;
-
-public class BR {
-  public static int _all = 0;
-}
